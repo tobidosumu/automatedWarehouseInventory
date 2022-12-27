1c48a121055b9ef14cac2002f1d84cf056960e3c
tobidosumu <dosunmumishael@gmail.com>
2022-12-27 14:01:15 +0100

    import 'calculateRowWeight' method from 'utilities folder

--- jack-warehouse/controllers/item.controller.js
+++ jack-warehouse/controllers/item.controller.js
@@ -1,20 +1,33 @@
 const itemModel = require("../models/item.model");
+const { calculateRowWeight } = require("../utilities/calculateRowWeight"); // Import the calculateRowWeight function from the utility module
 
 const itemController = {
   create: async (req, res) => {
-    // Set the response header
-    // res.set('Content-Type', 'application/json');
-    
-    const doc = new itemModel(req.body);
-    await doc.save((err, data) => {
-      console.log(err, data);
-      if (err) {
-        res.status(400).json({ status: "error trying to create item" });
+    try {
+      // Create a new item using the request body
+      const doc = new itemModel(req.body);
+
+      // Calculate the total weight of items in the row
+      const rowWeight = await calculateRowWeight(doc.row_num);
+
+      if (doc.row_num > 25) {
+        console.log(doc.row_num)
+        // Row number exceeds maximum of 25
+        res.status(400).json({ status: "Maximum number of rows exceeded 25 rows" });
       } else {
-        res.status(200).json({ status: "item added successfully", data });
+        if (rowWeight + doc.weight > 10000) {
+          // Row weight exceeds maximum capacity of 10 tonnes
+          res.status(400).json({ status: "Maximum weight capacity of row exceeded 10 tonnes" });
+        } else {
+          // Save the new item to the database
+          await doc.save();
+          // console.log(err, data);
+          res.status(200).json({ status: "item added successfully" });
+        }
       }
-    });
-    res.status(200).json({ status: "item added successfully" });
+    } catch (err) {
+      res.status(400).json({ status: "error trying to add item" });
+    }
   },
 
   update: async (req, res) => {
