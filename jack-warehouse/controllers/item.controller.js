65ef2c9c617cbb29eef60a652d5604aa73359618
tobidosumu <dosunmumishael@gmail.com>
2022-12-28 13:31:07 +0100

    getbyid method can now fetch an item from database

--- jack-warehouse/controllers/item.controller.js
+++ jack-warehouse/controllers/item.controller.js
@@ -35,58 +35,103 @@ const itemController = {
         res.status(200).json({ status: `${doc.name} added successfully` });
     }
     } catch (err) {
-      res.status(400).json({ error: err.message });
+      // Handle any errors that occur while finding the item
+      // res.status(400).json({ error: err.message });
+      res.status(400).json({ status: "Error occurred while adding item" });
     }
   },
-
+  
   // Get all items/objects from the database
   getall: async (req, res) => {
     try {
       const items = await itemModel.find();
       res.status(200).json({ status: "Items retrieved successfully", items });
     } catch (err) {
-      res.status(400).json({ error: err.message });
+      // Handle any errors that occur while finding the item
+      // res.status(400).json({ error: err.message });
+      res.status(400).json({ status: "Error occurred while retrieving all items" });
+
     }
   },
   
-
-  update: async (req, res) => {
-    res.status(200).json({ status: "item updated successfully" });
-  },
-
+  // Get an item by its id from the database
   getbyid: async (req, res) => {
     try {
       // Get the ObjectId from the request parameters
       const objectId = mongoose.Types.ObjectId(req.params.id);
-      
+      console.log(objectId)
       // Find the item with the matching ObjectId
-      const item = await Item.findOne({ _id: objectId });
-      
-      console.log(objectId);
-      console.log(item);
-  
+      const item = await itemModel.findById(objectId);
+
       // If the item was not found, return a 400 response
       if (!item) {
         res.status(400).json({ status: "Item not found" });
       } else {
-        // If the item was found, return it in the response  
+        // If the item was found, return it in the response
         res.status(200).json({ status: "Item retrieved by id successfully", item });
       }
     } catch (err) {
       // Handle any errors that occur while finding the item
-      // res.status(400).json({ status: "error trying to find item" });
+      res.status(400).json({ status: "Error occurred while retrieving item" });
+      // res.status(400).json({ error: err.message });
+    }
+  },
+
+  // Update an item detail using the _id field
+  update: async (req, res) => {
+    try {
+    // Find the item with the matching ObjectId
+    const item = await itemModel.findById(objectId);
+    
+    // If the item was not found, return a 400 response
+    if (!item) {
+      res.status(400).json({ status: "Item not found" });
+    } else {
+      // Update the item details with the request body
+      item.name = req.body.name;
+      item.weight = req.body.weight;
+      item.row_num = req.body.row_num;
+    
+      // Calculate the total weight of items in the row
+      const rowWeight = await calculateRowWeight(item.row_num);
+    
+      // Check if the updated item weight exceeds the row capacity
+      if (rowWeight + item.weight > rowCapacity) {
+        res.status(400).json({ status: `Row number (${item.row_num}) remaining storage space (${rowCapacity - rowWeight} tonnes) is less than ${item.name} weight (${item.weight} tonnes)` });
+      } else {
+        // Save the updated item to the database
+        await item.save();
+        res.status(200).json({ status: `${item.name} updated successfully` });
+      }
+    }
+    } catch (err) {
+      // Handle any errors that occur while updating the item
+      res.status(400).json({ error: err. Message });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      // Get the ObjectId from the request parameters
+      const objectId = mongoose.Types.ObjectId(req.params.id);
+  
+      // Delete the item with the matching ObjectId
+      const result = await itemModel.deleteOne({ _id: objectId });
+  
+      // If no items were deleted, return a 400 response
+      if (result.deletedCount === 0) {
+        res.status(400).json({ status: "Item not found" });
+      } else {
+        // If the item was deleted, return a 200 response
+        res.status(200).json({ status: "Item deleted successfully" });
+      }
+    } catch (err) {
+      // Handle any errors that occur while deleting the item
       res.status(400).json({ error: err.message });
+      // res.status(400).json({ status: "user not updated successfully" });
     }
   },
   
-
-  // getbyid: async (req, res) => {
-  //   res.status(200).json({ status: "item retrieved by id successfully" });
-  // },
-
-  delete: async (req, res) => {
-    res.status(200).json({ status: "item deleted" });
-  },
 };
 
 module.exports = itemController;
