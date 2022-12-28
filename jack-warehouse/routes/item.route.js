7ff63064912d2eb1067c70f0118d64c4c31a867a
tobidosumu <dosunmumishael@gmail.com>
2022-12-28 15:02:34 +0100

    fixed bug preventing delete method from working

--- jack-warehouse/routes/item.route.js
+++ jack-warehouse/routes/item.route.js
@@ -6,6 +6,6 @@ router.post("/create", itemController.create);
 router.get("/getall", itemController.getall);
 router.get("/getbyid/:id", itemController.getbyid);
 router.patch("/update/:id", itemController.update);
-router.delete("/delete", itemController.delete);
+router.delete("/delete/:id", itemController.delete);
 
 module.exports = router;
