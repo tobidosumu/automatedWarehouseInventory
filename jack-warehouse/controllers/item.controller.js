904a6ad67de22057490662a3044b4ecf3adc6abf
tobidosumu <dosunmumishael@gmail.com>
2022-12-30 12:31:40 +0100

    added data sanitization and Jack can now find items by their name

--- jack-warehouse/controllers/item.controller.js
+++ jack-warehouse/controllers/item.controller.js
@@ -9,6 +9,18 @@ const itemController = {
     try {  
       // Create a new item using the request body
       const doc = new itemModel(req.body);
+
+      // Convert the item name to lowercase
+      doc.name = doc.name
+      // Converts item name to lowercase
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-zA-Z]/g, ' ')
+      .replace(/\b(\w+)\b(?!\s)/g, '$1 ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/\s$/, '')
+      .replace(/\s(\w+\b)$/, '$1')
+      .replace(/^\s*(\w+\b)/, '$1');
     
       // Calculate the total weight of items in the row
       const rowWeight = await calculateRowWeight(doc.row_num);
@@ -131,83 +143,106 @@ const itemController = {
   // Update an item detail using the _id field
   update: async (req, res) => {
     try {
-      // Get the ObjectId from the request parameters
-      const objectId = mongoose.Types.ObjectId(req.params.id);
-      
-      // Find the item with the matching ObjectId
-      const item = await itemModel.findById(objectId);
-      
-      // If the item was not found, return a 400 response
-      if (!item) {
-        res.status(400).json({ status: "Item not found" });
-      } else {
-        // Update the item details with the request body
-        item.name = req.body.name;
-        item.weight = req.body.weight;
-        item.row_num = req.body.row_num;
-      
-        // Checks if updated item details are valid
-        switch (true) {
-          // Checks if no name is entered for item
-          case doc.name === "":
-            res.status(400).json({ status: "Please, enter item name" });
-          break; 
+    // Get the ObjectId from the request parameters
+    const objectId = mongoose.Types.ObjectId(req.params.id);
+    
+    // Find the item with the matching ObjectId
+    const item = await itemModel.findById(objectId);
+    
+    // If the item was not found, return a 400 response
+    if (!item) {
+      res.status(400).json({ status: "Item not found" });
+    } 
+    else {
+      // Update the item details with the request body
+      item.name = req.body.name
+      // Converts item name to lowercase
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-zA-Z]/g, ' ')
+      .replace(/\b(\w+)\b(?!\s)/g, '$1 ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/\s$/, '')
+      .replace(/\s(\w+\b)$/, '$1')
+      .replace(/^\s*(\w+\b)/, '$1');
 
-          // Checks if item weight is less than 1 tonne
-          case doc.weight < 1:
-            res.status(400).json({ status: "Item weight cannot be less than 1 tonne" });
-          break; 
+      item.weight = req.body.weight;
+      item.row_num = req.body.row_num;
+    
+      // Calculate the total weight of items in the row
+      const rowWeight = await calculateRowWeight(item.row_num);
+  
+      // Check if updated item details are valid
+      switch (true) {
+        // Checks if no name is entered for item
+        case item.name === "":
+          res.status(400).json({ status: "Please, enter item name" });
+        break; 
+    
+        // Checks if item weight is less than 1 tonne
+        case item.weight < 1:
+          res.status(400).json({ status: "Item weight cannot be less than 1 tonne" });
+        break;
+    
+        /* 
+          Checks if row number is less than 1.
+          Note: This validaton can be removed if a set of values starting from 1 
+          is provided for Jack/users to select from
+        */
+        case item.row_num < 1:
+          res.status(400).json({ status: "Row number cannot be less than 1" });
+        break;
 
-          /* 
-            Checks if row number is less than 1.
-            Note: This validaton can be removed if a set of values starting from 1. 
-            is provided for Jack/users to select from
-          */
-          case doc.row_num < 1:
-            res.status(400).json({ status: "Row number cannot be less than 1" });
-          break;
+        /* 
+          Checks if row number is less than 1.
+          Note: This validaton can be removed if a set of values starting from 1. 
+          is provided for Jack/users to select from
+        */
+        case item.row_num < 1:
+          res.status(400).json({ status: "Row number cannot be less than 1" });
+        break;
 
-          /* 
-            Checks if row number is less than 1.
-            Note: This validaton can be removed if a set of values ending at 25 
-            is provided for Jack/users to select from
-          */
-          case doc.row_num > 25:
-            res.status(400).json({ status: "Maximum number of rows exceeded (25 rows max)" });
-          break;
+        /* 
+          Checks if row number is less than 1.
+          Note: This validaton can be removed if a set of values ending at 25 
+          is provided for Jack/users to select from
+        */
+        case item.row_num > 25:
+          res.status(400).json({ status: "Maximum number of rows exceeded (25 rows max)" });
+        break;
 
-          /* 
-            Checks if production date is selected.
-            Note: Date can be extracted from calendar
-          */ 
-          case doc.production_date === null || doc.production_date === undefined:
-            res.status(400).json({ status: "Please enter production date" });
-          break;   
+        /* 
+          Checks if production date is selected.
+          Note: Date can be extracted from calendar
+        */ 
+        case item.production_date === null || item.production_date === undefined:
+          res.status(400).json({ status: "Please enter production date" });
+        break;   
 
-          /* 
-            Checks if expiry date is selected.
-            Note: Date can be extracted from calendar
-          */
-          case doc.expiry_date === null || doc.expiry_date === undefined:
-            res.status(400).json({ status: "Please enter expiry date" });
-          break;
+        /* 
+          Checks if expiry date is selected.
+          Note: Date can be extracted from calendar
+        */
+        case item.expiry_date === null || item.expiry_date === undefined:
+          res.status(400).json({ status: "Please enter expiry date" });
+        break;
 
-          // Checks if row capacity(10 tonnes/10,000kg) is exceeded
-          case rowWeight + doc.weight > rowCapacity:
-            res.status(400).json({ status: `Row number (${doc.row_num}) remaining storage space (${rowCapacity - rowWeight} tonnes) is less than ${doc.name} weight (${doc.weight} tonnes)` });
-          break;
+        // Checks if row capacity(10 tonnes/10,000kg) is exceeded
+        case rowWeight + item.weight > rowCapacity:
+          res.status(400).json({ status: `Row number (${item.row_num}) remaining storage space (${rowCapacity - rowWeight} tonnes) is less than ${item.name} weight (${item.weight} tonnes)` });
+        break;
 
-          // Checks if weight value or row value is numeric
-          case isNaN(doc.weight) || isNaN(doc.row_num):
-            res.status(400).json({ status: "Invalid value entered" });
-          break;
+        // Checks if weight value or row value is numeric
+        case isNaN(item.weight) || isNaN(item.row_num):
+          res.status(400).json({ status: "Invalid value entered" });
+        break;
 
-          default:
-          // Save new item to database if all checks are valid
-          await doc.save();
-          res.status(200).json({ status: `${doc.name} added successfully` });
-        }
+        default:
+        // Save new item to database if all checks are valid
+        await item.save();
+        res.status(200).json({ status: `${item.name} updated successfully` });
       }
+    }
     } catch (err) {
       /* Handles any errors that occur while updating item.
         Note: I prefer to use this error-handling format during dev 
@@ -337,7 +372,10 @@ const itemController = {
       // Calculate the remaining capacity by subtracting the row weight from the row capacity
       const remainingCapacity = rowCapacity - rowWeight;
 
-      res.status(200).json({ status: "Row capacity retrieved successfully", remainingCapacity  });
+      // res.status(200).json({ status: "Row capacity retrieved successfully", remainingCapacity });
+      res.status(200).json({ status: "Row capacity retrieved successfully", 
+      remainingCapacity: `${remainingCapacity}kg` });
+
     } 
     catch (err) {
       /* Handles any errors that occur while getting the remaining storage space of a specified row.
@@ -374,7 +412,52 @@ const itemController = {
       res.status(400).json({ error: err.message });
       // res.status(400).json({ status: "Error occurred while generating list of items in row" });
     }
+  },
+
+  // Method for finding items with same word in their names
+  getbyname: async (req, res) => {
+    try {
+
+      // Get the name from the request parameters
+      const name = req.params.name
+        // Converts item name to lowercase
+        .toLowerCase()
+        // Removes leading and trailing whitespace from item name
+        .trim()
+        // Removes all whitespace within item name (i.e. extra spaces between letters)
+        .replace(/\s+/g, '')
+        // Removes all special characters from item name
+        .replace(/[^\w\s]/gi, '')
+        // Adds a space between each word in item name // not working yet
+        .replace(/\b(\w+)\b/g, '$1 ')
+        // Replaces multiple spaces with a single space
+        .replace(/\s{2,}/g, ' '
+      );
+
+      console.log(name)
+      // Find items with names that contain the provided name
+      const items = await itemModel.find({ name: { $regex: name } });
+
+      if (items.length === 0) {
+        // If no items were found, return a 400 response
+        res.status(400).json({ status: "No items found with the specified name" });
+      } else {
+        // Return the found items in the response
+        res.status(200).json({ status: "Items retrieved by name successfully", items });
+      }
+
+    } 
+    catch (err) {
+      /* Handles any errors that occur while finding item(s).
+        Note: I prefer to use this error-handling format during dev 
+        as it provides useful debugging error message.
+      */
+      res.status(400).json({ error: err.message });
+      // res.status(400).json({ status: "Error occurred while finding item(s)" });
+    }
   }
+
+
 };
 
 module.exports = itemController;
