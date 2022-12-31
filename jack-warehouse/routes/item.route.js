11f9b05e363cddcdfd6d92582cb8ce8496b0d6ce
tobidosumu <dosunmumishael@gmail.com>
2022-12-31 14:22:39 +0100

    remove redundant code + add some comments

--- jack-warehouse/routes/item.route.js
+++ jack-warehouse/routes/item.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const itemController = require("../controllers/item.controller");
-const itemModel = require("../models/item.model");
 
+// For handling CRUD operations
 router.post("/create", itemController.create);
 router.get("/getall", itemController.getall);
 router.get("/getbyid/:id", itemController.getbyid);
 router.patch("/update/:id", itemController.update);
 router.delete("/delete/:id", itemController.delete);
 
+// For handling extra operations
 router.get("/gettotalweight", itemController.getTotalWeight);
 router.get("/averageweight/:row", itemController.getAverageWeight);
 router.get("/emptyrows", itemController.getEmptyRows);
